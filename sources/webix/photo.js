a5a9d3b016432a85f398b4ab009d3ede8c4f655c
xialer <xialer@gmail.com>
2022-04-01 20:43:05 +0800

    修复图片

--- sources/webix/photo.js
+++ sources/webix/photo.js
@@ -4,7 +4,7 @@ webix.protoUI({
 		return this.config.value;
 	},
 	setValue(value){
-		this.setHTML("<img style='height:260px;' webix_tooltip='The image of the Client' src='data/images/"+value+".jpg'>");
+		this.setHTML("<img style='height:260px;' webix_tooltip='The image of the Client' src='data/images/avatar/"+value+".jpg'>");
 		this.config.value = value;
 	}
 },
